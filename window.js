79f6540c54b69a6ab7881a1a4cd2e09ff50e2f13
tkv-29 <thanitrk@gmail.com>
2023-12-19 23:16:01 +0530

    stage demo

--- window.js
+++ window.js
@@ -1,4 +1,20 @@
-// Getting input via STDIN
+/*Assume you are a student studying in school.You are given a task to find first negative integer for each and every window of size k.
+
+Input Description:
+First line contains an integer n denoting the size of the array. The next line contains n space separated integers forming the array. The last line contains the window size k.
+
+Output Description:
+Print the first negative integer in that window.If all the numbers are positive print 0
+
+Sample Input :
+7
+1 -2 -3 -4 5 6 -7
+3
+Sample Output :
+-2 -2 -3 -4 -7
+*/
+
+
 const readline = require("readline");
 
 const inp = readline.createInterface({
