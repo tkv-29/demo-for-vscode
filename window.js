6de1c0b0ba1211e2f2823e4aad152078296f6cde
tkv-29 <thanitrk@gmail.com>
2023-12-19 23:34:10 +0530

    window problem updated

--- window.js
+++ window.js
@@ -15,6 +15,7 @@ Sample Output :
 */
 
 
+// Getting input via STDIN
 const readline = require("readline");
 
 const inp = readline.createInterface({
@@ -29,25 +30,29 @@ inp.on("line", (data) => {
 
 inp.on("close", () => {
 
-  console.log(userInput);
+//   console.log(userInput);
     let n = +userInput[0];
     let arr = userInput[1].split(' ');
     let k = +userInput[2];
     let nArray = arr.map(Number)
+    let opArray = [];
     // console.log(nArray)
     for(i=0 ; i<=(nArray.length-k) ; i++){
     // console.log(nArray[i],nArray[i+1],nArray[i+2])
     let tempArray = nArray.slice(i,i+k)
-     console.log(tempArray)
+    //  console.log(tempArray)
     for(j=0 ; j<=k ; j++){
         if(tempArray[j]<0){
-            console.log(tempArray[j])
+            opArray.push(tempArray[j])
             break;
         }else if(j==k-1){
-            console.log(0)
+            opArray.push(0)
         }
     }
-    }
     
+    }
+    console.log(opArray.join(' '))
     
 });
+
+// dummy 
